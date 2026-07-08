f6cbb8dfa32f52cea7337807f4bd998c3f857288
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-07-08 15:31:03 +0600

    16th commit

--- index.js
+++ index.js
@@ -137,6 +137,11 @@ app.patch('/my-requests/:id', async (req, res) => {
 app.get('/blood-requests', async (req, res) => {
   try {
     const { page = 1, limit = 10 } = req.query; // Default to page 1 and limit 10 if not provided
+     if (!page || !limit) {
+      const requests = await collection.find().toArray();
+
+      return res.json(requests);
+    }
     const skip = (Number(page) - 1) * Number(limit);
     const requests = await collection.find().skip(skip).limit(Number(limit)).toArray();
     const totalRequests = await collection.countDocuments();
